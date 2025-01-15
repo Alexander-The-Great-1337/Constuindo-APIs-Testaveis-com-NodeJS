0c2b203c61b52d0ec449642e04b00f35e435d96f
Ale Pereira <naitwa-alexandre@gmail.com>
2025-01-15 09:15:00 -0300

    lesson 01 IO operations

---
+++ Introduction/Io Async non-blocking operations/lesson01.js
@@ -0,0 +1,25 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+let fileContent;
+
+const someMath = 1 + 1;
+const filePath = path.resolve(__dirname, '../data/big-file.txt');
+
+fs.readFile(filePath, 'utf8', function (err, content) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(content);
+});
+
+try {
+  fileContent = fs.readFileSync(filePath, 'utf8');
+  console.log('file has been read');
+} catch (error) {
+  console.log(error);
+}
+
+const text = `The sum is ${someMath}`;
+
+console.log(text);
