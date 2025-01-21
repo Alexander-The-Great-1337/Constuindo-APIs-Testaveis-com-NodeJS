6b74625917cf3537d57f837fdcd1cb71c1fa9266
Ale Pereira <naitwa-alexandre@gmail.com>
2025-01-21 08:51:23 -0300

    middlewares

--- api/dist/server.cjs
+++ api/dist/server.cjs
@@ -5,7 +5,12 @@ var _bodyParser = _interopRequireDefault(require("body-parser"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
 const app = (0, _express.default)();
 app.use(_bodyParser.default.json());
+const myMiddleware = (req, res, next) => {
+  console.log('will run before any route');
+  next();
+};
 app.get('/', (req, res) => res.end('Hello World!'));
+app.use(myMiddleware);
 app.listen(3000, () => {
   console.log('Exemplo app listening on port 3000!');
 });
--- api/src/server.js
+++ api/src/server.js
@@ -4,7 +4,23 @@ import bodyParser from 'body-parser';
 const app = express();
 app.use(bodyParser.json());
 
+const myMiddleware = (req, res, next) => {
+  console.log('will run before any route');
+  next();
+}
+
+const errorHandler = (err, req, res, next) => {
+  console.log('Something went wrong');
+  res.status(500).send(err.message);
+}
+
+app.use('/users', myMiddleware);
+
+app.use(errorHandler);
+
 app.get('/', (req, res) => res.end('Hello World!'));
+app.get('/users', (req, res) => res.end('Hello Users!'));
+
 
 app.listen(3000, () => {
   console.log('Exemplo app listening on port 3000!');
