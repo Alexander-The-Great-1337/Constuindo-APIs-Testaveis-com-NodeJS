3ebf122054358e885d3d4b3f44f2df60dfa0ef3c
Ale Pereira <naitwa-alexandre@gmail.com>
2025-01-27 08:00:26 -0300

    starting integration tests

--- api/dist/server.cjs
+++ api/dist/server.cjs
@@ -9,8 +9,14 @@ const myMiddleware = (req, res, next) => {
   console.log('will run before any route');
   next();
 };
+const errorHandler = (err, req, res, next) => {
+  console.log('Something went wrong');
+  res.status(500).send(err.message);
+};
+app.use('/users', myMiddleware);
+app.use(errorHandler);
 app.get('/', (req, res) => res.end('Hello World!'));
-app.use(myMiddleware);
+app.get('/users', (req, res) => res.end('Hello Users!'));
 app.listen(3000, () => {
   console.log('Exemplo app listening on port 3000!');
 });
