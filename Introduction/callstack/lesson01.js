e0550cb05a4fe257f7b3447d887833c0c2ab1de3
Ale Pereira <naitwa-alexandre@gmail.com>
2025-02-01 10:15:27 -0300

    leson01 changes

--- Introduction/callstack/lesson01.js
+++ Introduction/callstack/lesson01.js
@@ -12,3 +12,25 @@ function generateUserDescription(name, surName, age) {
 const userInformation = generateUserDescription('Alexandre', 'Pereira', 34);
 
 console.log(userInformation);
+
+function solution(N, ratings) {
+    // You must complete the logic for the function that is provided
+    // before compiling or submitting to avoid an error.
+
+    // Write your code here
+
+    ratings = ratings.sort((a, b) => a[1] - b[1]);
+
+    let highiest = ratings[ratings.length - 1][1];
+
+    for (let i = 0; i < ratings.length; i++) {
+      if (ratings[i][1] >= highiest) {
+          console.log(ratings[i]);
+          highiest = ratings[i][0];
+        }
+    }
+
+    return highiest;
+}
+
+console.log(solution(2, [[987654171 , 3], [987654478, 3], [987654092,4], [987654421, 3], [987654001, 4]]));
