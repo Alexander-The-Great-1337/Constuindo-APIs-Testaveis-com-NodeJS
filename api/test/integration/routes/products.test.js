111e74de4eb213a07eab56d5c16e44ac2df8ffb4
Ale Pereira <naitwa-alexandre@gmail.com>
2025-02-24 07:58:38 -0300

    testing post route

--- api/test/integration/routes/products.test.js
+++ api/test/integration/routes/products.test.js
@@ -44,16 +44,42 @@ describe('Routes: Products', () => {
         });
     });
   });
-
-  describe('/products/:id when an id is specified', () => {
-    it('should return 200 with on project', done => {
-      request
-        .get(`/products/${defaultId}`)
-        .end((err, res) => {
-          expect(res.statusCode).to.eql(200);
-          expect(res.body).to.eql(expectedProduct);
-          done(err);
-        });
+  describe('GET /products/:id ', () => {
+    context('when an id is specified', () => {
+      it('should return 200 with on project', done => {
+        request
+          .get(`/products/${defaultId}`)
+          .end((err, res) => {
+            expect(res.statusCode).to.eql(200);
+            expect(res.body).to.eql(expectedProduct);
+            done(err);
+          });
+      });
     });
+  });
+  describe('POST /products', () => {
+    context('when posting a product', () => {
+      it('should return a new product with status 201', done => {
+        const customId = '56cb91bdc3464f14678934ba';
+        const newProduct = Object.assign({}, { _id: customId, __v: 0 }, defaultProduct);
+
+        const expectedSavedProduct = {
+          __v: 0,
+          _id: customId,
+          name: 'Default procuct',
+          description: 'product description',
+          price: 100,
+        }
+
+        request
+          .post('/products')
+          .send(newProduct)
+          .end((err, res) => {
+            expect(res.statusCode).to.eql(200);
+            expect(res.body).to.eql(expectedSavedProduct)
+            done(err);
+          });
+      });
+    })
   });
 });
