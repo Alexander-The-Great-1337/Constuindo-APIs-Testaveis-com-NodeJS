41eaee123105dd866ee23d94e09f58c674fb5674
Ale Pereira <naitwa-alexandre@gmail.com>
2025-05-03 20:05:15 -0300

    feat:testing

--- api/src/controllers/users.js
+++ api/src/controllers/users.js
@@ -7,6 +7,7 @@ class UsersController {
   async get(req, res) {
     try {
       const users = await this.User.find({});
+      
       res.send(users);
     } catch (error) {
       res.status(400).send(error.message);
